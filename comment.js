7d61c98e6c2d9ad128ec445698c595b9277e3af6
John Nazarene Dela Pisa <jannazarene09@gmail.com>
2023-12-25 07:30:55 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,24 +1,69 @@
 // Create web server
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
-// Import express
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/comment.controller');
+// Create web server
+var server = require('http').Server(app)
+var port = process.env.PORT || 3000
+server.listen(port, function () {
+  console.log('Server is running on port ' + port)
+})
 
-// Get all comment
-router.get('/', commentController.getComment);
+// Create socket io
+var io = require('socket.io')(server)
 
-// Get comment by id
-router.get('/:id', commentController.getCommentById);
+// Create mongoose
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/realtime_comment', { useNewUrlParser: true })
 
-// Create new comment
-router.post('/', commentController.createComment);
+// Create schema
+var commentSchema = new mongoose.Schema({
+  username: String,
+  content: String
+})
 
-// Update comment by id
-router.put('/:id', commentController.updateCommentById);
+// Create model
+var Comment = mongoose.model('Comment', commentSchema)
 
-// Delete comment by id
-router.delete('/:id', commentController.deleteCommentById);
+// Create static folder
+app.use(express.static('public'))
 
-// Export router
-module.exports = router;
+// Create body parser
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// Create route
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+// Create socket io
+io.on('connection', function (socket) {
+  console.log('User connected')
+  socket.on('disconnect', function () {
+    console.log('User disconnected')
+  })
+
+  socket.on('client-send-data', function (data) {
+    // console.log(data)
+    io.sockets.emit('server-send-data', data)
+  })
+})
+
+// Create route
+app.post('/comment', urlencodedParser, function (req, res) {
+  // console.log(req.body)
+  var newComment = Comment(req.body).save(function (err, data) {
+    if (err) throw err
+    io.sockets.emit('server-send-data', req.body)
+    res.json(data)
+  })
+})
+
+app.get('/comment', function (req, res) {
+  Comment.find({}, function (err, data) {
+    if (err) throw err
+    res.json(data)
+  })
+})
